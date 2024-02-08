e3096e2c6dc760217bf7c1aecbd60a80e305382d
Yab1 <yabilisanu@gmail.com>
2024-02-08 21:29:28 +0300

    Rendered Expandable

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import HeroSection from "@/components/landing_page/HeroSection";
+import { Expandable, HeroSection } from "@/components/landing_page";
 
 export default function LandingPage() {
   return (
     <main className="bg-black font-bold text-white">
       <HeroSection />
+      <Expandable />
     </main>
   );
 }
