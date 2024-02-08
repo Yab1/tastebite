48fc4fdf29b0573fb5bff93a7489834a86cf7360
Yab1 <yabilisanu@gmail.com>
2024-02-08 19:27:09 +0300

    Rendered HeroSection

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/landing_page/HeroSection";
+
+export default function LandingPage() {
+  return (
+    <main className="bg-black font-bold text-white">
+      <HeroSection />
+    </main>
+  );
+}
